4c19acc3a8b521105becd0c04d140ad328470a85
ashok_2216 <ashoksiva2216@gmail.com>
2022-07-04 11:56:10 +0530

    main.py file added

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import numpy as np
+import names
 
-import openpyxl as opxl
+name = []
+for i in range(10):
+    out = names.get_full_name()
+    name.append(out)
 
-workb = opxl.Workbook()
+df = pd.DataFrame({'Name':name,
+                   'Marks':np.random.randint(10, 100, dtype=int)})
 
-
-activeSheet = workb.active
-
-
-cell1 = activeSheet.cell(row=1, column=1)
-cell2 = activeSheet.cell(row=2, column=1)
-cell3 = activeSheet.cell(row=3, column=1)
-cell4 = activeSheet.cell(row=4, column=1)
-cell5 = activeSheet.cell(row=5, column=1)
-cell6 = activeSheet.cell(row=6, column=1)
-
-cell1.value = "Name"
-cell2.value = "Hi"
-cell3.value = "Apple"
-cell4.value = "Orange"
-cell5.value = "Good"
-cell6.value = "1000"
-
-cell3 = activeSheet['C1']
-cell3.value = "1000"
-
-cell3 = activeSheet['C2']
-cell3.value = "565656"
-
-
-workb.save("file1.xlsx")
+writer = ExcelWriter('output_file.xlsx')
+df.to_excel(writer,'Sheet1',index=False)
+writer.save()
